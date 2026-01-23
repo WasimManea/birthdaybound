289d4ec2ff25057ea60a835ac1354b5dac3402f9
WasimManea <109655171+WasimManea@users.noreply.github.com>
2026-01-23 20:20:07 +0400

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final TestMessageService service;
+
+     public HelloController(TestMessageService service) {
+        this.service = service;
+    }
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    public List<TestMessage> getMessages() {
+        return service.getAllMessages();
     }
 }
